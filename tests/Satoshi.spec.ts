4c836566b3e4ffaf7baa9495c8079da45a5bf88e
Vladimir Babin <vovababin@gmail.com>
2025-01-04 22:29:30 +0500

    Fix probability

--- tests/Satoshi.spec.ts
+++ tests/Satoshi.spec.ts
@@ -1,23 +1,23 @@
-import { Blockchain, BlockchainTransaction, SandboxContract, TreasuryContract } from '@ton/sandbox';
-import { toNano } from '@ton/core';
-import { Satoshi } from '../wrappers/Satoshi';
-import '@ton/test-utils';
+import { Blockchain, BlockchainTransaction, SandboxContract, TreasuryContract } from '@ton/sandbox'
+import { toNano } from '@ton/core'
+import { Satoshi } from '../wrappers/Satoshi'
+import '@ton/test-utils'
 
 describe('Satoshi', () => {
-    let blockchain: Blockchain;
-    let deployer: SandboxContract<TreasuryContract>;
-    let satoshi: SandboxContract<Satoshi>;
+    let blockchain: Blockchain
+    let deployer: SandboxContract<TreasuryContract>
+    let satoshi: SandboxContract<Satoshi>
 
     async function randomTreasury() {
-        let rnd = (Math.random() + 1).toString(36).substring(7);
-        return await blockchain.treasury(rnd);
+        let rnd = (Math.random() + 1).toString(36).substring(7)
+        return await blockchain.treasury(rnd)
     }
 
     function incrementBlockchainTime(seconds: number) {
         if (!blockchain.now) {
-            blockchain.now = Math.floor(Date.now() / 1000);
+            blockchain.now = Math.floor(Date.now() / 1000)
         }
-        blockchain.now += seconds;
+        blockchain.now += seconds
     }
 
     async function mine(treasury: SandboxContract<TreasuryContract>): Promise<BlockchainTransaction[]> {
@@ -29,12 +29,12 @@ describe('Satoshi', () => {
                 $$type: 'Mine',
                 receiver: treasury.address
             }
-        );
+        )
         return result.transactions
     }
 
     beforeEach(async () => {
-        blockchain = await Blockchain.create();
+        blockchain = await Blockchain.create()
         blockchain.verbosity = {
             blockchainLogs: false,
             vmLogs: 'none',
@@ -42,9 +42,9 @@ describe('Satoshi', () => {
             print: true
         }
 
-        satoshi = blockchain.openContract(await Satoshi.fromInit());
+        satoshi = blockchain.openContract(await Satoshi.fromInit())
 
-        deployer = await blockchain.treasury('deployer');
+        deployer = await blockchain.treasury('deployer')
 
         const deployResult = await satoshi.send(
             deployer.getSender(),
@@ -55,56 +55,74 @@ describe('Satoshi', () => {
                 $$type: 'Deploy',
                 queryId: 0n,
             }
-        );
+        )
 
         expect(deployResult.transactions).toHaveTransaction({
             from: deployer.address,
             to: satoshi.address,
             deploy: true,
             success: true,
-        });
-    });
+        })
+    })
 
     it('should deploy', async () => {
         // the check is done inside beforeEach
         // blockchain and satoshi are ready to use
-    });
+    })
 
     it('should get start mining data', async () => {
-        const data = (await satoshi.getGetMiningData());
-        const expectedSubsidy = toNano("50");
+        const data = (await satoshi.getGetMiningData())
+        const expectedSubsidy = toNano("50")
         expect(data).toEqual({
             "$$type": "MiningParams",
             last_block: 0n,
             current_subsidy: expectedSubsidy,
             last_block_time: data.last_block_time
-        });
-    });
+        })
+    })
+
+    it('should mine with low probability of success', async () => {
+        const wallet = await randomTreasury()
+        let attempts = 100
+        let successCount = 0
+        for (let i = 0; i < attempts; i++) {
+            incrementBlockchainTime(10)
+            const transactions = await mine(wallet)
+            if (transactions.length === 4) {
+                successCount++
+            }
+        }
+        const successRate = successCount / attempts
+        expect(successRate).toBeGreaterThan(0.005)
+        expect(successRate).toBeLessThan(0.015)
+    })
+
 
     it('should mine correctly', async () => {
-        let wallet = await randomTreasury();
-        let failedTrxs = await mine(wallet); // chance ~= 1%
-        expect(failedTrxs).toHaveLength(3);
-        expect((await satoshi.getGetJettonData()).totalSupply).toEqual(toNano(0));
+        let wallet = await randomTreasury()
+        let failedTrxs = await mine(wallet) // chance ~= 1%
+        expect(failedTrxs).toHaveLength(3)
+        expect((await satoshi.getGetJettonData()).totalSupply).toEqual(toNano(0))
 
-        incrementBlockchainTime(60 * 10);
+        incrementBlockchainTime(60 * 10)
 
-        let successTrxs = await mine(wallet); // chance = 100%
-        expect(successTrxs).toHaveLength(4);
-        expect((await satoshi.getGetJettonData()).totalSupply).toEqual(toNano(50));
-    });
+        let successTrxs = await mine(wallet) // chance = 100%
+        expect(successTrxs).toHaveLength(4)
+        expect((await satoshi.getGetJettonData()).totalSupply).toEqual(toNano(50))
+    })
 
     it('should mine predictable with halvings', async () => {
-        let wallet = await randomTreasury();
         const iterCount = 22
         const iterBlocks = 10000
         var shouldEqualSupply = 0n
+        incrementBlockchainTime(60 * 10 * Number(iterBlocks))
+        let wallet = await randomTreasury()
         for (let i = 1; i < iterCount; i++) {
-            incrementBlockchainTime(60 * 10 * Number(iterBlocks))
             await mine(wallet)
-            const data = (await satoshi.getGetMiningData())
-            shouldEqualSupply += BigInt(iterBlocks) * data.current_subsidy
+            incrementBlockchainTime(60 * 10 * Number(iterBlocks))
+            const miningData = (await satoshi.getGetMiningData())
+            shouldEqualSupply += BigInt(iterBlocks) * miningData.current_subsidy
             expect((await satoshi.getGetJettonData()).totalSupply).toEqual(shouldEqualSupply)
         }
-    });
-});
+    })
+})
