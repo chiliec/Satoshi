b12a0f447314d47533c9ae044877a10de6917ce0
Vladimir Babin <vovababin@gmail.com>
2025-01-04 16:01:43 +0500

    Calculate block subsidy

--- tests/Satoshi.spec.ts
+++ tests/Satoshi.spec.ts
@@ -1,5 +1,5 @@
 import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
-import { toNano } from '@ton/core';
+import { fromNano, toNano } from '@ton/core';
 import { Satoshi } from '../wrappers/Satoshi';
 import '@ton/test-utils';
 
@@ -11,7 +11,7 @@ describe('Satoshi', () => {
     beforeEach(async () => {
         blockchain = await Blockchain.create();
 
-        satoshi = blockchain.openContract(await Satoshi.fromInit(0n));
+        satoshi = blockchain.openContract(await Satoshi.fromInit());
 
         deployer = await blockchain.treasury('deployer');
 
@@ -39,44 +39,8 @@ describe('Satoshi', () => {
         // blockchain and satoshi are ready to use
     });
 
-    it('should increase counter', async () => {
-        const increaseTimes = 3;
-        for (let i = 0; i < increaseTimes; i++) {
-            console.log(`increase ${i + 1}/${increaseTimes}`);
-
-            const increaser = await blockchain.treasury('increaser' + i);
-
-            const counterBefore = await satoshi.getCounter();
-
-            console.log('counter before increasing', counterBefore);
-
-            const increaseBy = BigInt(Math.floor(Math.random() * 100));
-
-            console.log('increasing by', increaseBy);
-
-            const increaseResult = await satoshi.send(
-                increaser.getSender(),
-                {
-                    value: toNano('0.05'),
-                },
-                {
-                    $$type: 'Add',
-                    queryId: 0n,
-                    amount: increaseBy,
-                }
-            );
-
-            expect(increaseResult.transactions).toHaveTransaction({
-                from: increaser.address,
-                to: satoshi.address,
-                success: true,
-            });
-
-            const counterAfter = await satoshi.getCounter();
-
-            console.log('counter after increasing', counterAfter);
-
-            expect(counterAfter).toBe(counterBefore + increaseBy);
-        }
+    it('should check original subsidy', async () => {
+        const subsidy = (await satoshi.getGetParams()).current_subsidy;
+        expect(fromNano(subsidy)).toEqual("50");
     });
 });
