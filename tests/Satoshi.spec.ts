747ae4abee51162e2569e132df00d19c7401190d
Vladimir Babin <vovababin@gmail.com>
2025-01-04 18:47:33 +0500

    Calculate probability

--- tests/Satoshi.spec.ts
+++ tests/Satoshi.spec.ts
@@ -1,4 +1,4 @@
-import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
+import { Blockchain, BlockchainTransaction, SandboxContract, TreasuryContract } from '@ton/sandbox';
 import { fromNano, toNano } from '@ton/core';
 import { Satoshi } from '../wrappers/Satoshi';
 import '@ton/test-utils';
@@ -8,8 +8,39 @@ describe('Satoshi', () => {
     let deployer: SandboxContract<TreasuryContract>;
     let satoshi: SandboxContract<Satoshi>;
 
+    async function randomTreasury() {
+        let rnd = (Math.random() + 1).toString(36).substring(7);
+        return await blockchain.treasury(rnd);
+    }
+
+    function incrementBlockchainTime(seconds: number) {
+        if (!blockchain.now) {
+            blockchain.now = Math.floor(Date.now() / 1000);
+        }
+        blockchain.now += seconds;
+    }
+
+    async function mine(treasury: SandboxContract<TreasuryContract>): Promise<BlockchainTransaction[]> {
+        const result = await satoshi.send(treasury.getSender(),
+            {
+                value: toNano("0.12"),
+            },
+            {
+                $$type: 'Mine',
+                receiver: treasury.address
+            }
+        );
+        return result.transactions
+    }
+
     beforeEach(async () => {
         blockchain = await Blockchain.create();
+        blockchain.verbosity = {
+            blockchainLogs: false,
+            vmLogs: 'none',
+            debugLogs: true,
+            print: true
+        }
 
         satoshi = blockchain.openContract(await Satoshi.fromInit());
 
