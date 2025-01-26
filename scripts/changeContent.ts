c44d30ed255db83b2fcccc3961d86fb3a5e92ed8
Vladimir Babin <vovababin@gmail.com>
2025-01-26 12:16:31 +0500

    Add change content script

---
+++ scripts/changeContent.ts
@@ -0,0 +1,41 @@
+import { toNano, Address } from '@ton/core';
+import { Satoshi } from '../wrappers/Satoshi';
+import { NetworkProvider } from '@ton/blueprint';
+import { buildOnchainMetadata } from '../utils/jetton-helpers';
+
+export async function run(provider: NetworkProvider) {
+
+    const ui = provider.ui();
+
+    const contractAddress = await ui.input('Please enter contract address:');
+
+    const satoshi = provider.open(Satoshi.fromAddress(Address.parse(contractAddress)));
+
+    const name = await ui.input('Please enter jetton name:');
+    const description = await ui.input('Please enter jetton description:');
+    const symbol = await ui.input('Please enter jetton symbol:');
+    const image = await ui.input('Please enter link to jetton image:');
+
+    const jettonParams = {
+        name: name,
+        description: description,
+        symbol: symbol,
+        image: image,
+    }
+    const content = buildOnchainMetadata(jettonParams);
+
+    await satoshi.send(
+        provider.sender(),
+        {
+            value: toNano('0.01'),
+        },
+        {
+            $$type: 'TokenUpdateContent',
+            content: content,
+        }
+    );
+
+    await provider.waitForDeploy(satoshi.address);
+
+    console.log('Content changed for', satoshi.address);
+}
